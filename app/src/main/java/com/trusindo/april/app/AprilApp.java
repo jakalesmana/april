393166bdf18558f8ccdca43b99662fc7ce6f9d1c
jakalesmana <jaka.lesmana@mainspring.technology>
2017-10-20 19:45:25 +0700

    update multidexapplication

--- app/src/main/java/com/trusindo/april/app/AprilApp.java
+++ app/src/main/java/com/trusindo/april/app/AprilApp.java
@@ -1,12 +1,13 @@
 package com.trusindo.april.app;
 
 import android.app.Application;
+import android.support.multidex.MultiDexApplication;
 
 /**
  * Created by jakalesmana on 10/14/17.
  */
 
-public class AprilApp extends Application {
+public class AprilApp extends MultiDexApplication {
 
     @Override
     public void onCreate() {
